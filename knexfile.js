9fe5369030e242dcffbc6f1c314763a13e6ab1d3
Juan Aleman <juan.j.aleman@gmail.com>
2020-02-09 21:51:52 -0600

    knex files set up

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,12 @@ module.exports = {
     connection: {
       filename: "./data/projects.db3"
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 };
